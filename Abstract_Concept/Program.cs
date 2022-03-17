52d42a2854602d32916d111d2b08000e6a94fd11
Arati121 <avbhosale@mitaoe.ac.in>
2022-03-17 10:46:26 +0530

    Add project files.

---
+++ Abstract_Concept/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Abstract_Concept
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(4);
+            c1.CalculateArea();
+            Console.WriteLine(c1);
+
+            Reactangle r1 = new Reactangle(4, 5);
+            r1.CalculateArea();
+            Console.WriteLine(r1);
+        }
+    }
+}
---
+++ Abstract_Concept/Shape.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_Concept
+{
+  abstract public  class Shape
+    {
+        abstract public void CalculateArea();
+    }
+    public class Circle : Shape
+    {
+        private int radius;
+        double result, PI = 3.14;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public override void CalculateArea()
+        {
+            result = PI * radius * radius;
+        }
+        public override string ToString()
+        {
+            return "Area of circle= " + result;
+        }
+    }
+        public class Reactangle : Shape
+        {
+            private int length;
+            private int breath;
+            private int result;
+            public Reactangle(int length,int breath)
+            {
+                this.length = length;
+                this.breath = breath;
+            }
+
+            public override void CalculateArea()
+            {
+                result = length * breath;
+            }
+            public override string ToString()
+            {
+                return "Area of reactangle= " + result;
+            }
+        
+    }
+}
